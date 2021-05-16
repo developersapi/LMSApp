8de2e0d0ed72973ed842c4d192e9cca5902e321f
lucas.silva676 <lucas.silva676@fatec.sp.gov.br>
2021-05-16 15:46:42 -0300

    arquivos do app uploads

--- upload/urls.py
+++ upload/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from mysite.core import views
+from upload.core import views
 
 
 urlpatterns = [
